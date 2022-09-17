7d976cfae17d48a241bfe9b71c6173fac97180e8
duynghia-123 <duynghia22302@gmail.com>
2022-09-17 15:44:43 +0700

    finish interface

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -5,9 +5,11 @@ import MedicalFacility from "./Section/MedicalFacility";
 import OutStandingDoctor from "./Section/OutStandingDoctor";
 import Specialty from "./Section/Specialty";
 import HandBook from "./Section/HandBook";
+import About from "./Section/About";
 import "./HomePage.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import HomeFooter from "./HomeFooter";
 
 class HomePage extends Component {
   render() {
@@ -25,6 +27,8 @@ class HomePage extends Component {
         <MedicalFacility settings={settings} />
         <OutStandingDoctor settings={settings} />
         <HandBook settings={settings} />
+        <About />
+        <HomeFooter />
       </div>
     );
   }
