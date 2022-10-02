ff25a37cb251b1ebb62fbf28431120224d2e153a
duynghia-123 <duynghia22302@gmail.com>
2022-10-02 14:27:09 +0700

    Tạo Dữ Liệu Các Cơ Sở Y Tế/Phòng Khám Bệnh

--- src/services/userService.js
+++ src/services/userService.js
@@ -86,6 +86,10 @@ const getAllDetailSpecialtyById = (data) => {
   );
 };
 
+const createNewClinic = (data) => {
+  return axios.post("/api/create-new-clinic", data);
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -106,4 +110,5 @@ export {
   createNewSpecialty,
   getAllSpecialty,
   getAllDetailSpecialtyById,
+  createNewClinic,
 };
