feab2b2de58b6b35b288a50ded9ddff31a50a7b1
duynghia-123 <duynghia22302@gmail.com>
2022-10-04 04:10:40 +0700

    Tạo Base Giao Diện Quản Lý Danh Sách Lịch Hẹn Khám Bệnh Cho Bác Sỹ

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -80,6 +80,11 @@ export const doctorMenu = [
         name: "menu.doctor.manage-schedule",
         link: "/doctor/manage-schedule",
       },
+      {
+        // quản lý bệnh nhân khám bệnh của bác sỹ
+        name: "menu.doctor.manage-patient",
+        link: "/doctor/manage-patient",
+      },
     ],
   },
 ];
